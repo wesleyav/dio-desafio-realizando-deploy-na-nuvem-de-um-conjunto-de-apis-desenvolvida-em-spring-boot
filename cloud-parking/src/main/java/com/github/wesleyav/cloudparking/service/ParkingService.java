0d818e690d7633e0cc30ada693470b5f5ba6ad93
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-08 13:33:44 -0300

    Implementando os testes

--- cloud-parking/src/main/java/com/github/wesleyav/cloudparking/service/ParkingService.java
+++ cloud-parking/src/main/java/com/github/wesleyav/cloudparking/service/ParkingService.java
@@ -17,14 +17,11 @@ public class ParkingService {
 
 	private static Map<String, Parking> parkingMap = new HashMap<>();
 
-	static {
-		var id = getUUID();
-		var id1 = getUUID();
-		Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
-		Parking parking1 = new Parking(id1, "WAS-1234", "SP", "VW GOL", "VERMELHO");
-		parkingMap.put(id, parking);
-		parkingMap.put(id1, parking1);
-	}
+//	static {
+//		var id = getUUID();
+//		Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
+//		parkingMap.put(id, parking);
+//	}
 
 	public List<Parking> findAll() {
 
--- cloud-parking/src/test/java/com/github/wesleyav/cloudparking/ParkingControllerIT.java
+++ cloud-parking/src/test/java/com/github/wesleyav/cloudparking/ParkingControllerIT.java
@@ -1,9 +1,14 @@
 package com.github.wesleyav.cloudparking;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import com.github.wesleyav.cloudparking.controller.dto.ParkingCreateDTO;
 
 import io.restassured.RestAssured;
 
@@ -23,12 +28,34 @@ public class ParkingControllerIT {
 	}
 
 	@Test
-	void findAll() {
-
+	void whenFindAllThenCheckResult() {
+		RestAssured.given()
+		.when()
+		.get("/parking")
+		.then()
+		.statusCode(HttpStatus.OK.value());
 	}
 
 	@Test
-	void create() {
+	void whenCreateThenCheckIsCreated() {
+		
+		ParkingCreateDTO createDTO = new ParkingCreateDTO();
+		createDTO.setColor("AMERELO");
+		createDTO.setLicense("WRT-5555");
+		createDTO.setModel("BRASILIA");
+		createDTO.setState("SP");
+		
+		RestAssured.given()
+		.when()
+		.contentType(MediaType.APPLICATION_JSON_VALUE)
+		.body(createDTO)
+		.post("/parking")
+		.then()
+		.statusCode(HttpStatus.CREATED.value())
+		.body("license", Matchers.equalTo("WRT-5555"))
+		.body("color", Matchers.equalTo("AMARELO"));
+		
+		
 
 	}
 
